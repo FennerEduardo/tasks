8867a74e209531b4ff1c49f6a0f317dea16f501b
Fenner Eduardo González Castellanos <fennereduardo@gmail.com>
2023-11-20 17:15:41 -0500

    :fire: First commit

---
+++ src/test/java/fenner/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package fenner.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
